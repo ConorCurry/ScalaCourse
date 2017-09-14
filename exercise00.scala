7ffb87cd2de61fb175412b9ddca4dccb073c474e
Conor Curry <conorcurry@gmail.com>
2017-09-14 16:05:20 -0400

    Add .class files to gitignore, and fix syntax in exercise00.

--- exercise00.scala
+++ exercise00.scala
@@ -1,7 +1,9 @@
-def and(x: Boolean, y: => Boolean): Boolean = {
-  if !x false else y
-}
+object exercise00 {
+  def and(x: Boolean, y: => Boolean): Boolean = {
+    if(!x) false else y
+  }
 
-def or(x: Boolean, y: => Boolean): Boolean = {
-  if x true else y
+  def or(x: Boolean, y: => Boolean): Boolean = {
+    if(x) true else y
+  }
 }
