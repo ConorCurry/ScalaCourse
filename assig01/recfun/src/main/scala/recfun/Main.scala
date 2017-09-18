61413f44d57c124a4558624ad328e010fb4d8e8f
Conor Curry <conorcurry@gmail.com>
2017-09-18 00:08:15 -0400

    Final implementations -- all tests passing

--- assig01/recfun/src/main/scala/recfun/Main.scala
+++ assig01/recfun/src/main/scala/recfun/Main.scala
@@ -1,5 +1,7 @@
 package recfun
 
+import scala.annotation.tailrec
+
 object Main {
   def main(args: Array[String]) {
     println("Pascal's Triangle")
@@ -19,10 +21,28 @@ object Main {
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ???
-  
+    def balance(chars: List[Char]): Boolean = {
+      @tailrec
+      def balance_aux(parens: Int, chars: List[Char]): Boolean = {
+        if (chars.isEmpty) parens == 0
+        else if (chars.head == '(') balance_aux(parens + 1, chars.tail)
+        else if (chars.head == ')') (parens > 0) && balance_aux(parens - 1, chars.tail)
+        else balance_aux(parens, chars.tail)
+      }
+
+      balance_aux(0, chars)
+    }
+
   /**
    * Exercise 3
    */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+    def countChange(money: Int, coins: List[Int]): Int = {
+      def change(money: Int, coins: List[Int]): Int = {
+        if (money == 0) 1
+        else if (money > 0 && !coins.isEmpty) change(money - coins.head, coins) + change(money, coins.tail)
+        else 0
+      }
+
+      if (money > 0) change(money, coins) else 0
+    }
   }
